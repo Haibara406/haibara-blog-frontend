ecacd4a0e13e97d3a560ea4a45c871d8a10eb4bb
haibara <haibara406@gmail.com>
2025-08-14 19:59:33 +0800

    修复里程碑页面*号错误替代日期的错误

--- haibara-blog/src/components/Layout/SideBar/ChargingList/index.vue
+++ haibara-blog/src/components/Layout/SideBar/ChargingList/index.vue
@@ -6,7 +6,7 @@ const achievementList = ref([
   {
     name: "起点",
     money: "2024-03-20",
-    message: "在CSDN发布了第一篇博客，并收获了2000+阅读量，9点赞和12收藏"
+    message: "在CSDN发布了第一篇博客"
   },
   {
     name: "再续",
@@ -15,18 +15,18 @@ const achievementList = ref([
   },
   {
     name: "缘起",
-    money: "2024-04-24",
+    money: "2024-04-14",
     message: "在Gitee上解锁第一次代码提交"
   },
   {
     name: "曦月",
-    money: "2024-07-12",
+    money: "2024-04-18",
     message: "在Github上解锁第一次代码提交"
   },
   {
     name: "萌芽",
     money: "2024-11-09",
-    message: "除此有了想做一个属于自己的网站的想法，但由于彼时实力一般，搁浅了.."
+    message: "有了想做一个个人网站的想法，但由于彼时实力一般，搁浅了.."
   },
   {
     name: "初鸣",
@@ -41,7 +41,7 @@ const achievementList = ref([
   {
     name: "破晓",
     money: "2025-08-11",
-    message: "后端正式部署在服务器上"
+    message: "第一版后端正式部署在服务器上"
   },
   {
     name: "充盈",
@@ -56,15 +56,14 @@ const achievementList = ref([
   {
     name: "远航",
     money: "直至今日",
-    message: "迈向更广阔的未来，梦想在路上持续闪耀"
+    message: "至此迈向更广阔的未来，梦想在路上持续闪耀"
   }
 ])
 </script>
 
 <template>
   <div>
-    <!-- TODO 重构榜单-->
-    <Card title="个人里程碑" prefix-icon="edit" :isScale="true">
+    <Card title="里程碑" prefix-icon="edit" :isScale="true">
       <div class="text-[12px] text-left">
         在这段旅程中，我用心记录下个人与网站成长的每一个珍贵瞬间。这里不仅是冷冰冰的数据堆砌，更是一段段饱含欢笑、泪水与坚持的故事。每一次进步，都是岁月留下的温暖印记；每一次回望，都是对初心的深情呼唤。感谢一路陪伴、鼓励和支持我的每一位朋友，因为有你们，这段旅途变得更加丰富而美好。让我们一同见证，这个小小角落如何在时光中逐渐生长，绽放属于它的光芒。
         <br>（以下榜单时间排序）
@@ -80,7 +79,7 @@ const achievementList = ref([
         <div class="item-div">
           <div>日期</div>
           <hr>
-          <div v-for="charging in achievementList" :key="charging.name">{{ '*'.repeat(charging.money.length) }}</div>
+          <div v-for="charging in achievementList" :key="charging.name">{{charging.money}}</div>
         </div>
         <div class="item-div">
           <div>备注</div>
@@ -107,7 +106,7 @@ const achievementList = ref([
         </div>
       </div>
       <div class="text-left text-[12px] mt-2 font-bold">
-        <span class="text-[#999]">新的成就，等你见证</span>
+        <span class="text-[#999]">新的成就，邀你见证</span>
       </div>
     </Card>
   </div>
